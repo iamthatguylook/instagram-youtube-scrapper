5bc73c4f725be857b3adf6f132c2cc9222a271a6
vjtrollkilla <vjsingh284@gmail.com>
2023-01-20 22:00:07 +0400

    Unable to find the video svg even when its there error

--- scrapingInstagramSelenium.py
+++ scrapingInstagramSelenium.py
@@ -55,17 +55,25 @@ for followee in  followingProfiles:
         
         iteratorFollowing +=1
     #doubleClickAction.keyDown(Keys.LEFT_CONTROL).click(followee) #doubleClickAction.double_click(followee).perform() to perform double clicks
-
-for iterateTabs in range(followingNumber):
+postVideos = []
+videoLinks= []
+for iterateTabs in range(1,followingNumber):
     
     childWindow = driver.window_handles[iterateTabs]
     driver.switch_to.window(childWindow)
     driver.execute_script("window.scrollTo(0,4000);")
-    postVideos = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.TAG_NAME,"img")))
-    videos = [post.get_attribute("src") for post in postVideos]
-    print(videos)
-    if videos is None:
-        continue
+    
+    postVideos = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH,"//a[@class='x1i10hfl xjbqb8w x6umtig x1b1mbwd xaqea5y xav7gou x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz _a6hd']")))
+    for post in postVideos:
+        if post.find_element(By.XPATH,".//svg[@aria-label='Clip']") is True:
+            videoLinks.append(post.get_attribute('href'))
+            print(videoLinks)
+        else:
+            continue
+    print(videoLinks)
+    #print(vid
+    #if videos is None:
+    #    continue
     
         
     time.sleep(5)
